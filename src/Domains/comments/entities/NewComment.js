fbd4b937979e4528e9013a06673f8f416400c987
mzaedan <zaedanm108@gmail.com>
2024-07-30 09:16:56 +0700

    update

--- src/Domains/comments/entities/NewComment.js
+++ src/Domains/comments/entities/NewComment.js
@@ -2,17 +2,19 @@ class NewComment {
   constructor(payload) {
     this._verifyPayload(payload);
 
+    this.threadId = payload.threadId;
     this.content = payload.content;
+    this.owner = payload.owner;
   }
 
   _verifyPayload(payload) {
-    const { content } = payload;
+    const { threadId, content, owner } = payload;
 
-    if (!content) {
+    if (!threadId || !content || !owner) {
       throw new Error('NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof content !== 'string') {
+    if (typeof threadId !== 'string' || typeof content !== 'string' || typeof owner !== 'string') {
       throw new Error('NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
